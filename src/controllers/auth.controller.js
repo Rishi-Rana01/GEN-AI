09619b63886079543cf758c7123ef437e6b6ec82
Rishi Rana <rishirana012354@gmail.com>
2026-04-07 21:23:00 +0530

    implement user registration controller with input validation and duplicate check

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,26 @@ import userModel from '../models/user.model';
 
 
 async function registerUserController(req, res) {
-    
+    const {username, email, password} = req.body
+
+    if(!username || !email || !password){
+        return res.status(400).json({message: "All fields are required"})
+    }
+
+    isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    if(isUserAlreadyExist){
+        return res.status(400).json({message: "User already exist"})
+    }
+
+    const user = await userModel.create({username, email, password})
+
+    return res.status(201).json({message: "User created successfully", user})
 }
 
 
