82e3f1b856ba308bee0ef9a529cc1cd815d6be84
Rishi Rana <rishirana012354@gmail.com>
2026-04-09 15:47:23 +0530

    implement user registration and login authentication controllers and routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,37 +1,72 @@
 import userModel from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
-
+//register user controller
 async function registerUserController(req, res) {
-    const {username, email, password} = req.body
+    const { username, email, password } = req.body
 
-    if(!username || !email || !password){
-        return res.status(400).json({message: "All fields are required"})
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" })
     }
 
     isUserAlreadyExist = await userModel.findOne({
         $or: [
-            {username},
-            {email}
+            { username },
+            { email }
         ]
     })
 
-    if(isUserAlreadyExist){
-        return res.status(400).json({message: "User already exist"})
+    if (isUserAlreadyExist) {
+        return res.status(400).json({ message: "User already exist" })
     }
 
     const hash = await bcrypt.hash(password, 10)
 
-    const user = await userModel.create({username, email, password: hash})
+    const user = await userModel.create({ username, email, password: hash })
 
-    const token = jwt.sign({id: user._id, username: user.username}, process.env.JWT_SECRET, {expiresIn: "1d"})
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1d" })
 
     res.cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000
     })
 
-    return res.status(201).json({message: "User created successfully", user: {id: user._id, username: user.username, email: user.email}})
+    return res.status(201).json({ message: "User created successfully", user: { id: user._id, username: user.username, email: user.email } })
+}
+
+//login user controller
+async function loginUserController(req, res) {
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if (!isUserAlreadyExist) {
+        return res.status(400).json({ message: "User not found" })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, isUserAlreadyExist.password)
+
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: "Invalid password" })
+
+    }
+
+    const token = jwt.sign({ id: isUserAlreadyExist._id, username: isUserAlreadyExist.username }, process.env.JWT_SECRET, { expiresIn: "1d" })
+
+    res.cookie("token", token, {
+        maxAge: 24 * 60 * 60 * 1000
+    })
+
+    return res.status(200).json({ message: "User logged in successfully", user: { id: isUserAlreadyExist._id, username: isUserAlreadyExist.username, email: isUserAlreadyExist.email } })
 }
 
 
-export default {registerUserController}
+export default { registerUserController, loginUserController }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,7 @@ import authController from '../controllers/auth.controller.js'
 const authRouter = express.Router()
 
 authRouter.post('/register', authController.registerUserController)
+authRouter.post("/login", authController.loginUserController)
 
 
 export default authRouter
