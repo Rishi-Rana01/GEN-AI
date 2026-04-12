85cb163c07f5c8a76ed86aebdc50f00881f2dd9f
Rishi Rana <rishirana012354@gmail.com>
2026-04-12 17:28:20 +0530

    frontend project with React, Vite, routing, and authentication services

--- Frontend/src/appRoutes.jsx
+++ Frontend/src/appRoutes.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router'
 import Login from './features/auth/pages/login'
 import Register from './features/auth/pages/Register'
+import { Link } from 'react-router'
 
 const appRoutes = createBrowserRouter([
     {
         path: "/",
-        element: <div>Home</div>,
+        element: <div>Home
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </div>,
     },
     {
         path: "/login",
