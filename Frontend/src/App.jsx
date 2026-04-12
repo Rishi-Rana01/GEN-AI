3d93dcf707ea94327ffa3e6b2059be0c44fab19e
Rishi Rana <rishirana012354@gmail.com>
2026-04-12 17:51:23 +0530

    AuthContext and wrap application with AuthProvider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { RouterProvider } from 'react-router'
 import appRoutes from './appRoutes.jsx'
+import { AuthProvider } from './features/auth/authContext.jsx'
 function App() {
 
 
   return (
     <>
+      <AuthProvider>
+        <RouterProvider router={appRoutes} />
+      </AuthProvider>
 
-      <RouterProvider router={appRoutes} /> 
-      
 
     </>
   )
